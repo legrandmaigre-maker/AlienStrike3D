fac5e3acbf652c4817332d5e68941b8237f065aa
legrandmaigre-maker <legrandmaigre@gmail.com>
2025-12-31 10:09:23 +0100

    Add AlienStrike3D game code from Private_equity_tracker repo

---
+++ AlienStrike3D/AlienStrike3DApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct AlienStrike3DApp: App {
+      var body: some Scene {
+                WindowGroup {
+                              ContentView()
+                                  .preferredColorScheme(.dark)
+                }
+      }
+}
